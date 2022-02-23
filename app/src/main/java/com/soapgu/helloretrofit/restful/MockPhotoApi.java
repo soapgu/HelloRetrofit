b3abedd03fe1e88f425401d661af37838e70bf96
soapgu <ghost_gu@163.com>
2022-02-23 17:29:59 +0800

    1 add fack excetion 1 add convertStream from output to output

--- app/src/main/java/com/soapgu/helloretrofit/restful/MockPhotoApi.java
+++ app/src/main/java/com/soapgu/helloretrofit/restful/MockPhotoApi.java
@@ -9,16 +9,22 @@ import com.soapgu.helloretrofit.R;
 import com.soapgu.helloretrofit.models.Photo;
 import com.soapgu.helloretrofit.models.PhotoUrl;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 
 import io.reactivex.rxjava3.core.Single;
+import okhttp3.MediaType;
 import okhttp3.ResponseBody;
 import okhttp3.internal.http.RealResponseBody;
 import okio.BufferedSource;
 import okio.Okio;
 import okio.Source;
+import retrofit2.Response;
 import retrofit2.mock.BehaviorDelegate;
+import retrofit2.mock.Calls;
 
 public class MockPhotoApi implements PhotoApi{
     private final BehaviorDelegate<PhotoApi> delegate;
@@ -48,11 +54,60 @@ public class MockPhotoApi implements PhotoApi{
         Bitmap bitmap = BitmapFactory.decodeResource(this.context.getResources(), R.drawable.picture);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG , 100 ,stream );
-        byte[] contents = stream.toByteArray();
-        ByteArrayInputStream bis = new ByteArrayInputStream(contents);
-        Source source = Okio.source(bis);
+        int length = stream.size();
+        InputStream in;
+        try {
+            in = this.convertStream(stream);
+        }
+        catch (Exception ex){
+            return fakeException(ex);
+        }
+        Source source = Okio.source(in);
         BufferedSource bufferedSource = Okio.buffer(source);
-        ResponseBody body = new RealResponseBody("image/jpeg",contents.length,bufferedSource);
+        ResponseBody body = new RealResponseBody("image/jpeg",length,bufferedSource);
         return this.delegate.returningResponse(body).getImageFile(url);
     }
+
+    private InputStream convertStream( ByteArrayOutputStream source ) throws IOException {
+        int length = source.size();
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        new Thread(new Runnable() {
+            public void run () {
+                try {
+                    // write the original OutputStream to the PipedOutputStream
+                    // note that in order for the below method to work, you need
+                    // to ensure that the data has finished writing to the
+                    // ByteArrayOutputStream
+                    source.writeTo(out);
+                }
+                catch (IOException e) {
+                    // logging and exception handling should go here
+                }
+                finally {
+                    // close the PipedOutputStream here because we're done writing data
+                    // once this thread has completed its run
+                    if (out != null) {
+                        // close the PipedOutputStream cleanly
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+        return in;
+    }
+
+    private Single<ResponseBody> fakeException(Exception exception){
+        return this.delegate.returning(Calls.failure( new Exception("on no!!!") )).getImageFile("");
+    }
+
+    private Single<ResponseBody> getImageFile404(String url){
+        Response response = Response.error(404,ResponseBody.create(MediaType.parse("application/json"),""));
+        return this.delegate.returning(Calls.response(response)).getImageFile(url);
+    }
+
 }
